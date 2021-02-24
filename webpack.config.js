4988d5c5baf458fdae704c401be28636ef9ce808
Murod Khaydarov <murod.haydarov@gmail.com>
2021-02-24 19:59:03 +0300

    tools skeleton

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ],
+        exclude: /node_modules/,
+
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'NestedList',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+};
