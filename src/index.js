d4191a199b22a8237276df2a91589505f33be730
Robin@PiedWeb <contact@piedweb.com>
2021-05-08 11:28:53 +0200

    Permit to export to text a nested list

--- src/index.js
+++ src/index.js
@@ -108,6 +108,53 @@ export default class NestedList {
      */
     this.caret = new Caret();
   }
+  
+    /**
+   * Allow List Tool to be converted to/from other block
+   *
+   * @returns {{export: Function, import: Function}}
+   */
+  static get conversionConfig() {
+    return {
+      /**
+       * To create exported string from list, concatenate items by dot-symbol.
+       *
+       * @param {ListData} data - list data to create a string from thats
+       * @returns {string}
+       */
+      export: (data) => {
+        console.log(data.items);
+        return NestedList.itemsToText(data.items);
+      },
+      /**
+       * To create a list from other block's string, just put it at the first item
+       *
+       * @param {string} string - string to create list tool data from that
+       * @returns {ListData}
+       */
+      import: (string) => {
+        return {
+          items: [{ content: string, items: [] }],
+          style: "unordered",
+        };
+      },
+    };
+  }
+
+  static itemsToText(items) {
+    var text = "";
+    var nestedText = "";
+
+    items.forEach(function (item) {
+      text += (text ? ", " : "") + item.content;
+      if (item.items) {
+        nestedText = NestedList.itemsToText(item.items);
+        if (nestedText) text += (text ? ", " : "") + nestedText;
+      }
+    });
+
+    return text;
+  }
 
   /**
    * Returns list tag with items
