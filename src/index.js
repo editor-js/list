ef7adfe9c2e58a8eeafb5be265b63a66ff381008
Dan Frankowski <dfrankow@gmail.com>
2021-06-18 08:54:03 -0500

    onPaste from https://github.com/editor-js/nested-list/pull/22

--- src/index.js
+++ src/index.js
@@ -778,4 +778,75 @@ export default class NestedList {
      */
     this.unshiftItem();
   }
+
+  /**
+   * On paste callback that is fired from Editor
+   *
+   * @param {PasteEvent} event - event with pasted data
+   */
+  onPaste(event) {
+    const list = event.detail.data;
+
+    this.data = this.pasteHandler(list);
+
+    const oldView = this.nodes.wrapper;
+    if (oldView) {
+      oldView.parentNode.replaceChild(this.render(), oldView);
+    }
+  }
+
+  /**
+   * List Tool on paste configuration
+   *
+   * @public
+   */
+  static get pasteConfig() {
+    return {
+      tags: ["OL", "UL", "LI"],
+    };
+  }
+
+  /**
+   * Handle UL, OL and LI tags paste and returns List data
+   *
+   * @param {HTMLUListElement|HTMLOListElement|HTMLLIElement} element
+   * @returns {ListData}
+   */
+  pasteHandler(element) {
+    const { tagName: tag } = element;
+    let style;
+
+    switch (tag) {
+      case "OL":
+        style = "ordered";
+        break;
+      case "UL":
+      case "LI":
+        style = "unordered";
+    }
+
+    const data = {
+      style,
+      items: [],
+    };
+
+    if (tag === "LI") {
+      data.items.push({ content: element.innerHTML, items: [] });
+    } else {
+      const items = Array.from(element.querySelectorAll("LI"));
+
+      const listItems = items
+        .map((li) => li.innerHTML)
+        .filter((item) => !!item.trim());
+
+      const that = this;
+
+      listItems.forEach(function (item) {
+        data.items.push({ content: item, items: [] });
+        that.createItem(item, []);
+      });
+    }
+
+    return data;
+  }
 }
