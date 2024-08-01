7cbd0d77dee144fe3208710cf26f61ced703f904
e11sy <Egoramurin@gmail.com>
2024-08-01 22:11:24 +0300

    separated list renderer classes

---
+++ src/ListRenderer/listRenderer.ts
@@ -0,0 +1,46 @@
+/**
+ * CSS classes for the Nested List Tool
+ */
+interface NestedListCssClasses {
+  wrapper: string;
+  wrapperOrdered: string;
+  wrapperUnordered: string;
+  item: string;
+  itemBody: string;
+  itemContent: string;
+  itemChildren: string;
+  settingsWrapper: string;
+  itemChecked: string;
+  noHover: string;
+  checkbox: string;
+  checkboxContainer: string;
+}
+
+/**
+ * List renderer class
+ * Used for storing css classes and
+ */
+abstract class ListRenderer {
+  /**
+   * Styles
+   *
+   * @returns {NestedListCssClasses} - CSS classes names by keys
+   * @private
+   */
+  protected get CSS(): NestedListCssClasses {
+    return {
+      wrapper: 'cdx-nested-list',
+      wrapperOrdered: 'cdx-nested-list--ordered',
+      wrapperUnordered: 'cdx-nested-list--unordered',
+      item: 'cdx-nested-list__item',
+      itemBody: 'cdx-nested-list__item-body',
+      itemContent: 'cdx-nested-list__item-content',
+      itemChildren: 'cdx-nested-list__item-children',
+      settingsWrapper: 'cdx-nested-list__settings',
+      itemChecked: 'cdx-nested-list__item--checked',
+      noHover: 'cdx-nested-list__item-checkbox--no-hover',
+      checkbox: 'cdx-nested-list__item-checkbox-check',
+      checkboxContainer: 'cdx-nested-list__item-checkbox'
+    };
+  }
+}
