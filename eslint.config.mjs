0e71f28a611af92ddfb648ade82ce1d07d7dd103
e11sy <130844513+e11sy@users.noreply.github.com>
2024-09-28 22:13:54 +0300

    remove .eslintrc
    - removed .eslintrc
    - started eslint flatconfig setup

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+import CodeX from 'eslint-config-codex';
+import { plugin as TsPlugin, parser as TsParser } from 'typescript-eslint';
+
+export default [
+  ...CodeX,
+  {
+    name: 'editorjs-table',
+    ignores: ['eslint.config.mjs'],
+    plugins: {
+      '@typescript-eslint': TsPlugin,
+    },
+
+    /**
+     * This are the options for typescript files
+     */
+    languageOptions: {
+      parser: TsParser,
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: './',
+        sourceType: 'module', // Allows for the use of imports
+      },
+    },
+
+    rules: {
+      'n/no-missing-import': ['off'],
+      'n/no-unpublished-import': ['error', {
+        allowModules: ['vitest', 'postgres-migrations', 'eslint-config-codex'],
+        ignoreTypeImport: true,
+      }],
+      'n/no-unsupported-features/node-builtins': ['error', {
+        version: '>=22.1.0',
+      }],
+      'n/no-extraneous-import': ['error', {
+        allowModules: ['@editorjs/editorjs'],
+      }],
+      '@typescript-eslint/naming-convention': ['error', {
+        selector: 'property',
+        format: ['camelCase'],
+        filter: {
+          regex: '^(?!(2xx|2[0-9][0-9]|application/json|VITE.*|HAWK.*)$).*',
+          match: true,
+        },
+      }],
+
+      /**
+       * @todo get rid of this rule ignores and solve all eslint errors occured
+       */
+      '@typescript-eslint/no-unsafe-assignment': ['off'],
+      '@typescript-eslint/no-unsafe-argument': ['off'],
+      '@typescript-eslint/no-unsafe-return': ['off'],
+      '@typescript-eslint/no-unsafe-call': ['off'],
+      '@typescript-eslint/no-unsafe-member-access': ['off'],
+      'jsdoc/require-param-type': ['off'],
+      'jsdoc/informative-docs': ['off'],
+      'jsdoc/require-jsdoc': ['off'],
+    },
+  },
+
+  /**
+   * This override of the options for vue files
+   */
+  {
+    languageOptions: {
+      parser: VueParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        extraFileExtensions: ['.vue'],
+        parser: TsParser,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'n/no-missing-import': ['off'],
+      'n/no-unpublished-import': ['error', {
+        allowModules: ['vitest', 'postgres-migrations', 'eslint-import-resolver-alias', 'eslint-config-codex'],
+        ignoreTypeImport: true,
+      }],
+      'n/no-unsupported-features/node-builtins': ['error', {
+        version: '>=22.1.0',
+      }],
+      'jsdoc/require-param-type': ['off'],
+      'jsdoc/informative-docs': ['off'],
+      'jsdoc/require-jsdoc': ['off'],
+    },
+  },
+];
