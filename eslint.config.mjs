391546c73609360144a80140ec9054b5cfc9edf1
e11sy <130844513+e11sy@users.noreply.github.com>
2024-10-03 13:02:11 +0300

    fixed some of ts eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,6 @@ export default [
   ...CodeX,
   {
     name: 'editorjs-nested-list',
-    ignores: ['eslint.config.mjs'],
     plugins: {
       '@typescript-eslint': TsPlugin,
     },
@@ -25,35 +24,15 @@ export default [
     rules: {
       'n/no-missing-import': ['off'],
       'n/no-unpublished-import': ['error', {
-        allowModules: ['vitest', 'postgres-migrations', 'eslint-config-codex'],
+        allowModules: ['eslint-config-codex'],
         ignoreTypeImport: true,
       }],
       'n/no-unsupported-features/node-builtins': ['error', {
-        version: '>=22.1.0',
+        version: '>=20.11.1',
       }],
       'n/no-extraneous-import': ['error', {
-        allowModules: ['@editorjs/editorjs'],
+        allowModules: ['typescript-eslint'],
       }],
-      '@typescript-eslint/naming-convention': ['error', {
-        selector: 'property',
-        format: ['camelCase'],
-        filter: {
-          regex: '^(?!(2xx|2[0-9][0-9]|application/json|VITE.*|HAWK.*)$).*',
-          match: true,
-        },
-      }],
-
-      /**
-       * @todo get rid of this rule ignores and solve all eslint errors occured
-       */
-      '@typescript-eslint/no-unsafe-assignment': ['off'],
-      '@typescript-eslint/no-unsafe-argument': ['off'],
-      '@typescript-eslint/no-unsafe-return': ['off'],
-      '@typescript-eslint/no-unsafe-call': ['off'],
-      '@typescript-eslint/no-unsafe-member-access': ['off'],
-      'jsdoc/require-param-type': ['off'],
-      'jsdoc/informative-docs': ['off'],
-      'jsdoc/require-jsdoc': ['off'],
     },
   },
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import path from "path";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import * as pkg from "./package.json";
-import dts from "vite-plugin-dts";
+import path from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import * as pkg from './package.json';
+import dts from 'vite-plugin-dts';
 
-const NODE_ENV = process.argv.mode || "development";
+// eslint-disable-next-line n/prefer-global/process
+const NODE_ENV = process.argv.mode || 'development';
 const VERSION = pkg.version;
 
 export default {
   build: {
     copyPublicDir: false,
     lib: {
-      entry: path.resolve(__dirname, "src", "index.ts"),
-      name: "NestedList",
-      fileName: "nested-list",
+      // eslint-disable-next-line no-undef
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      name: 'NestedList',
+      fileName: 'nested-list',
     },
   },
   define: {
